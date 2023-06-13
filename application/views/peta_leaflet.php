41f214fc42151cc2f548e6713d1225af2a5056a7
ditadianto <adiantodita@upi.edu>
2023-06-14 04:51:50 +0700

    menambahkan prov jabar

--- application/views/peta_leaflet.php
+++ application/views/peta_leaflet.php
@@ -2,12 +2,16 @@
  <div id="map" style="width: 100%; height: 530px; color:black;"></div> 
 </div> 
 <script> 
+
+var provin = new L.LayerGroup();
+
 var map = L.map('map', { 
  center: [-1.7912604466772375, 116.42311966554416], 
  zoom: 5, 
  zoomControl: false,
  layers:[] 
 }); 
+
 var GoogleSatelliteHybrid= L.tileLayer('https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}', { 
 maxZoom: 22, 
 attribution: 'Latihan Web GIS' 
@@ -18,12 +22,29 @@ var OpenStreetMap_DE = L.tileLayer('https://{s}.tile.openstreetmap.de/{z}/{x}/{y
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 
+var Stamen_Terrain = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}{r}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 18,
+	ext: 'png'
+});
+
 var baseLayers = {
     'Google Satellite Hybrid': GoogleSatelliteHybrid,
-    'Open Street Map' : OpenStreetMap_DE
+    'Open Street Map' : OpenStreetMap_DE,
+    'Terrain' : Stamen_Terrain
 }; 
+
+var groupedOverlays = {
+    "Peta Dasar" : {
+        'Provinsi Jawa Barat' : provin
+    }
+};
+
 var overlayLayers = {} 
-L.control.layers(baseLayers, overlayLayers, {collapsed: true}).addTo(map);
+
+L.control.groupedLayers(baseLayers, groupedOverlays).addTo(map);
 
 var 
 osmUrl='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'; 
@@ -86,5 +107,43 @@ var div = L.DomUtil.create("div", "info legend");
 div.innerHTML = '<img src="<?=base_url()?>assets/arah-mata-angin.png"style=width:200px;>'; 
 return div; } 
 north.addTo(map);
+
+
+$.getJSON("<?=base_url()?>/assets/jabar.geojson",function(Kode){
+ L.geoJson( Kode, {
+ style: function(feature){
+ var fillColor,
+ Kode = feature.properties.Kode;
+ if ( Kode > 21 ) fillColor = "#006837"; 
+ else if (Kode>20) fillColor="#fec44f"
+ else if (Kode>19) fillColor="#c2e699"
+ else if (Kode>18) fillColor="#fee0d2"
+ else if (Kode>17) fillColor="#756bb1"
+ else if (Kode>16) fillColor="#8c510a"
+ else if (Kode>15) fillColor="#01665e"
+ else if (Kode>14) fillColor="#e41a1c"
+ else if (Kode>13) fillColor="#636363"
+ else if (Kode>12) fillColor= "#762a83"
+ else if (Kode>11) fillColor="#1b7837"
+ else if (Kode>10) fillColor="#d53e4f"
+ else if (Kode>9) fillColor="#67001f"
+ else if (Kode>8) fillColor="#c994c7"
+ else if (Kode>7) fillColor="#fdbb84"
+ else if (Kode>6) fillColor="#dd1c77"
+ else if (Kode>5) fillColor="#3182bd"
+ else if ( Kode > 4 ) fillColor ="#f03b20"
+ else if ( Kode > 3 ) fillColor = "#31a354";
+ else if ( Kode > 2 ) fillColor = "#78c679";
+ else if ( Kode > 1 ) fillColor = "#c2e699";
+ else if ( Kode > 0 ) fillColor = "#ffffcc";
+ else fillColor = "#f7f7f7"; // no data
+ return { color: "#999", weight: 1, fillColor: fillColor, fillOpacity: .6 };
+ },
+ onEachFeature: function( feature, layer ){
+ layer.bindPopup(feature.properties.WADMKK)
+ }
+ }).addTo(provin);
+ });
+
 
 </script>
